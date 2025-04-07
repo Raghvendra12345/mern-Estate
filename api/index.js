3877258072ea7100ee1e071b060eb12c3c8df713
Raghvendra12345 <103117995+Raghvendra12345@users.noreply.github.com>
2025-04-07 22:46:20 +0530

    create a middleware and functions

--- api/index.js
+++ api/index.js
@@ -1,16 +1,46 @@
     import express from 'express'
     // "test": "echo \"Error: no test specified\" && exit 1"    
-    import mongoose from 'mongoose'
-    import dotenv from "dotenv"
-
+    import mongoose from 'mongoose';
+    import dotenv from "dotenv";
+    import userRouter from './routes/user.routes.js';
+   import authRouter from './routes/auth.route.js';
+//    import connectDB from './db/connect.js'
     dotenv.config();
-    mongoose.connect("mongodb+srv://Raghav123:Raghav@cluster0.ynvkd.mongodb.net/").then(()=>{
-        console.log("connected mongodb")
+
+    // console.log("MongoDB URI:", process.env.MONGO);
+    mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("connected mongodb");
     })
     .catch((err)=>{
     console.log("not connected",err)
     })
     const app=express();
+
+    app.use(express.json())
+
+
+
+    app.get("/test",(req,res)=>{
+        //   res.send("Hello World")
+        res.json({
+            message:"Hello World!",
+        })
+    })
+    app.use("/api/user",userRouter)
+    app.use("/api/auth",authRouter)
     app.listen(3000,()=>{
         console.log("server is running fine !**")
+    })
+
+
+    //creating middleware
+    app.use((err,req,res,next)=>{
+        const statusCode=err.statusCode || 500;
+        const message=err.message || 'Internal Server Error';
+        return res.status(statusCode).json({
+            success:false,
+            statusCode,
+            message,
+        })
+
     })
---
+++ api/utils/error.js
@@ -0,0 +1,8 @@
+///this is made to throw error because if the password is not a certain length then it throw error
+
+export const errorHandler=(statusCode,message)=>{
+    const error=new Error()
+    error.statusCode=statusCode;
+    error.message=message;
+    throw error;
+}
